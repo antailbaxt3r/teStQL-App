2e50b37193bd3d76c3100b5b556fa53ea845ca72
antailbaxt3r <arjunbajpaiwork@gmail.com>
2020-05-06 16:43:19 +0530

    Added delete function

--- android_client/app/src/main/java/com/antailbaxt3r/gettestsapplication/activities/ViewTestsActivity.kt
+++ android_client/app/src/main/java/com/antailbaxt3r/gettestsapplication/activities/ViewTestsActivity.kt
@@ -1,13 +1,20 @@
 package com.antailbaxt3r.gettestsapplication.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentStatePagerAdapter
-import androidx.viewpager.widget.ViewPager
 import com.antailbaxt3r.gettestsapplication.R
+import com.antailbaxt3r.gettestsapplication.models.TestModel
+import com.antailbaxt3r.gettestsapplication.retrofit.RetrofitClient
 import com.antailbaxt3r.gettestsapplication.utils.ViewPagerAdapter
-import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_view_tests.*
+import net.steamcrafted.loadtoast.LoadToast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ViewTestsActivity : AppCompatActivity() {
 
@@ -15,9 +22,48 @@ class ViewTestsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_tests)
-
+        val loadToast = LoadToast(this)
+        loadToast.setText("Please wait...")
         setViewPager()
 
+        delete_all.setOnClickListener{
+
+            val builder = AlertDialog.Builder(this)
+            //set title for alert dialog
+            builder.setTitle("Confirm Delete")
+            //set message for alert dialog
+            builder.setMessage("Are you sure you want to delete all tests?")
+
+            //performing positive action
+            builder.setPositiveButton("Yes"){ _, _ ->
+                val call: Call<TestModel> = RetrofitClient.getClient().deleteAll()
+                call.enqueue(object: Callback<TestModel>{
+                    override fun onFailure(call: Call<TestModel>, t: Throwable) {
+                        Toast.makeText(applicationContext, "Something went wrong!", Toast.LENGTH_SHORT).show()
+                        t.printStackTrace()
+                        loadToast.error()
+                    }
+
+                    override fun onResponse(call: Call<TestModel>, response: Response<TestModel>) {
+                        if(response.isSuccessful && response.code() == 200){
+                            loadToast.success()
+                            finish()
+                            startActivity(intent)
+                        }else{
+                            loadToast.error()
+                        }
+                    }
+
+                })
+            }
+
+            //performing negative action
+            builder.setNegativeButton("Cancel"){ _, _ ->
+            }
+            // Create the AlertDialog
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.show()
+        }
     }
 
     private fun setViewPager() {
